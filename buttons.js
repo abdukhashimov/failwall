b30b82ce589b1f548cca01684d7eeef6b7d33ac3
Madiyor <abdukhashimov@yandex.ru>
2020-11-08 19:43:39 +0500

    added buttons

---
+++ buttons.js
@@ -0,0 +1,11 @@
+export const backEnd = "Back End";
+export const backEndAction = "backend";
+
+export const frontEnd = "Front End";
+export const frontEndAction = "frontend";
+
+export const mobile = "Mobile";
+export const mobileAction = "mobile";
+
+export const just = "Just a Bug";
+export const justAction = "just";
--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 require("dotenv").config();
-const { Telegraf } = require("telegraf");
+const { Telegraf, Markup } = require("telegraf");
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
+const inlineMessageMarkup = Markup.inlineKeyboard([
+  Markup.callbackButton('')
+])
+
 bot.start((ctx) => {
-  ctx.reply("Welcome");
+  ctx.reply("Welcome", );
 });
 
 bot.launch();
 // const mongoUrl =
 //   "mongodb+srv://madiyor:<password>@cluster0.jpxt2.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority";
+
+
+/**
+ * 1. BackEndBug
+ * 2. FrontEndBug
+ * 3. Just a Bug
+ * 
+ * 1. The tool name you were using
+ * 2. Failure you had
+ * 3. What did you learn?
+ * 
+ * 1. First Name Lastname
+ */
