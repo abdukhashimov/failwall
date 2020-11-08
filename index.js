3112b2e28bd58c91bdf3a12626b9137a929a7653
Madiyor <abdukhashimov@yandex.ru>
2020-11-08 19:26:14 +0500

    good start

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 require("dotenv").config();
+const { Telegraf } = require("telegraf");
 
-const mongoUrl =
-  "mongodb+srv://madiyor:<password>@cluster0.jpxt2.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+bot.start((ctx) => {
+  ctx.replyWithVideo("./welcome.mp4");
+});
+
+// const mongoUrl =
+//   "mongodb+srv://madiyor:<password>@cluster0.jpxt2.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority";
